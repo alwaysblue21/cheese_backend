32f4818a36b89ab30443794b5008021f8929db2f
Je Yun <alwaysblue21@gmail.com>
2023-08-05 15:47:07 -0400

    some middlewares

--- server.js
+++ server.js
@@ -1,23 +1,36 @@
 // dependencies
 //---------------------//
-//for .env
+// for .env
 require("dotenv").config();
 const { PORT = 5000, DATABASE_URL } = process.env;
-//for express
+// for express
 const express = require("express");
-//for app object
+// for app object
 const app = express();
 // for mongoose
 const mongoose = require("mongoose");
+// for cors
+const cors = require("cors");
+// for morgan
+const morgan = require("morgan");
 
 //establish connection
 mongoose.connect(DATABASE_URL);
 
-//connection events
+// connection events
 mongoose.connection
 .on("open", () => console.log("You are connected to mongoose"))
 .on("close", () => console.log("You are disconnected from mongoose"))
 .on("error", (error) => console.log(error))
+
+// middleware
+//---------------------//
+// cors for preventing cors errors...(allows all requests from other origins)
+app.use(cors())
+// morgan
+app.use(morgan("dev"))
+// json, express functionality to recognize incoming request objects as json objects
+app.use(express.json())
 
 // routes
 //---------------------//
